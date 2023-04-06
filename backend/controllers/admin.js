4e7c91b1ffa73b9dc46d0dd49a50fd0be1f0c85b
RahmeniRaoui <rahmeniraoui@gmail.com>
2023-04-06 21:16:40 +0200

    working on the back

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -10,3 +10,33 @@ exports.createAdmin = async function (req, res) {
     console.log(err);
   }
 };
+exports.fetchAdmin = async function (req, res) {
+  try {
+    const Admin = await db.find({});
+    res.status(201).send(Admin);
+  } catch (err) {
+    console.log(err);
+  }
+};
+exports.delete = async function (req, res) {
+  const id = req.params.id;
+  try {
+    const Admin = await db.findByIdAndDelete(id);
+    res.status(201).send("deleted", Admin);
+  } catch (err) {
+    console.log(err);
+  }
+};
+exports.update = async function (req, res) {
+  const id = req.params.id;
+  try {
+    const Admin = await db.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(201).send(Admin);
+  } catch (err) {
+    console.log(err);
+  }
+};
